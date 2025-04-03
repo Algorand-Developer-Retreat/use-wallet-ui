ef18806c14f723c16a8c9a0ecbf7f89233106506
Doug Richar <drichar@gmail.com>
2025-04-03 12:49:37 +0200

    feat(example): add minimal header layout to example project

--- examples/react/src/App.tsx
+++ examples/react/src/App.tsx
@@ -23,10 +23,27 @@ const walletManager = new WalletManager({
 function App() {
   return (
     <WalletProvider manager={walletManager}>
-      <div className="flex flex-col items-center justify-center h-screen dark:bg-[#001324]">
-        <WalletDialog>
-          <ConnectWalletButton />
-        </WalletDialog>
+      <div className="min-h-screen dark:bg-[#001324]">
+        {/* Header */}
+        <header className="w-full bg-gray-50 dark:bg-gray-800/30">
+          <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex justify-between items-center h-16">
+              <div className="flex-shrink-0">
+                {/* Logo or site name goes here */}
+              </div>
+              <div>
+                <WalletDialog>
+                  <ConnectWalletButton />
+                </WalletDialog>
+              </div>
+            </div>
+          </div>
+        </header>
+
+        {/* Content area */}
+        <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+          {/* Content goes here */}
+        </main>
       </div>
     </WalletProvider>
   )
